522d93793989d0afb1355600ffa53e1e7b790c17
PERCENTZERO <percentzero@outlook.com>
2023-06-03 12:10:11 +0900

    네비바 링크 추가

--- component/nav.tsx
+++ component/nav.tsx
@@ -1,25 +1,38 @@
 "use client";
 import styles from "@/styles/nav.module.css";
 import Link from "next/link";
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 export default function Nav() {
-  const navRef = useRef<HTMLDivElement>(null);
-
   useEffect(() => {
-    navRef.current?.addEventListener("click", (e) => {
-      if (e.target === navRef.current) {
-        navRef.current?.classList.toggle("active");
+    const nav = document.querySelector(`.${styles.container}`);
+    nav?.addEventListener("click", (e) => {
+      if (e.target === nav) {
+        nav?.classList.toggle("active");
       } else {
-        navRef.current?.classList.remove("active");
+        nav?.classList.remove("active");
       }
     });
   });
 
   return (
-    <div ref={navRef} className={styles.container}>
+    <div className={styles.container}>
       <Link href="/">
         <h1 className={styles.logo}>PERCENTZERO</h1>
       </Link>
+      <div className={styles.navItems}>
+        <Link href="/about">
+          <h1 className={styles.navItem}>더 알아보기</h1>
+        </Link>
+        <Link href="/portfolio">
+          <h1 className={styles.navItem}>포트폴리오</h1>
+        </Link>
+        <Link href="/contact">
+          <h1 className={styles.navItem}>연락</h1>
+        </Link>
+        <Link href="https://blog.0x0p.ml">
+          <h1 className={styles.navItem}>블로그</h1>
+        </Link>
+      </div>
     </div>
   );
 }
