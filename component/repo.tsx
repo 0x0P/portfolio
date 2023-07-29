53edf58b7760492f541d87aa533c135d9befc2eb
PERCENTZERO <percentzero@outlook.com>
2023-07-30 01:20:10 +0900

    시간변경

--- component/repo.tsx
+++ component/repo.tsx
@@ -2,9 +2,9 @@
 import Image from "next/image";
 import styles from "@/styles/repo.module.css";
 
-export default function Repo({ title, text, src, link, number }: { title: string; text?: string; src?: string; link?: string; number?: number }) {
+export default function Repo({ title, text, link, number }: { title: string; text: string; link: string; number?: number }) {
   return (
-    <div className={styles.repo} data-aos="fade-up" data-aos-delay={(number as any) * 50 + 100}>
+    <div className={styles.repo} data-aos="fade-up" data-aos-delay={(number as any) * 70 + 100}>
       <h1 className={styles.title}>{title}</h1>
       <h1 className={styles.text}>{text}</h1>
     </div>
